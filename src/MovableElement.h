12396cc81ffd66229ee8097ec622f5e9d8460391
Rémi Lasvenes <remi.lasvenes@etu.u-bordeaux.fr>
2016-04-05 18:07:14 +0200

    Modif classe attributs

--- src/MovableElement.h
+++ src/MovableElement.h
@@ -3,8 +3,27 @@
 
 class MovableElement
 {
+
+private:
+
+    int _x;
+    int _y;
+
+    int _width;
+    int _height;
+
+    int _deltaX;
+    int _deltaY;
+
 public:
+
     MovableElement();
+
+    void setDeltaX(int deltaX);
+    void setDeltaY(int deltaY);
+
+    int getPositionX() const;
+    int getPositionY() const;
 };
 
 #endif // MOVABLEELEMENT_H
